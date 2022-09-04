8def24948c1da86a58e61fa9ff784839346474e6
kimheera <hrk0295@naver.com>
2022-09-05 01:01:39 +0900

    second commit

--- src/main/java/com/example/lab1/Word.java
+++ src/main/java/com/example/lab1/Word.java
@@ -44,4 +44,15 @@ public class Word {
     public String getMeaning() {
         return meaning;
     }
+
+    @Override
+    public String toString() {
+        String slevel = "";
+        for (int i=0 ; i<level ; i++) slevel += "*";
+
+        String str = String.format("%-3s", slevel)
+                        + String.format("%15s", word) + "  " + meaning;
+
+        return str;
+    }
 }
--- src/main/java/com/example/lab1/WordCRUD.java
+++ src/main/java/com/example/lab1/WordCRUD.java
@@ -1,10 +1,35 @@
 package com.example.lab1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<Word>();
+        this.s = s;
+    }
 
     @Override
     public Object add() {
-        return null;
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.next();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord(){
+        Word one = (Word)add();
+        list.add(one);
+
+        System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
 
     @Override
@@ -20,5 +45,16 @@ public class WordCRUD implements ICRUD{
     @Override
     public void selectOne(int id) {
 
+    }
+
+    public void listAll(){
+        System.out.println("------------------------------");
+
+        for (int i=0 ; i<list.size() ; i++) {
+            System.out.print((i+1) + " ");
+            System.out.println(list.get(i).toString());
+        }
+
+        System.out.println("------------------------------");
     }
 }
